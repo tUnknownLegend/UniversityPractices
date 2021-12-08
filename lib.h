ebb044d7e3a98f4e03cdf3ae4676301ccae30b6c
Vladislav <57019979+tUnknownLegend@users.noreply.github.com>
2021-12-08 12:38:14 +0300

    set, no sort, working

---
+++ lib.h
@@ -0,0 +1,88 @@
+﻿#pragma once
+#include<iostream> 
+#include <set>
+#include <fstream> 
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+// Пары координат (x; y) 
+#define Pair pair<double, double>
+// Точность сравнения double
+#define accuracy 10e-10
+// Входной файл для Quick
+#define inFileQuick "problem2.txt"
+// Входной файл для NonEff
+#define inFileNonEff "problem2.txt"
+// Выходной файл Quick
+#define outFileQuick "out_Quick.txt"
+// Выходной файл Noneff
+#define outFileNonEff "out_NonEff.txt"
+
+// Хранит результат, т.е. выпуклую оболочку
+set<Pair> hull;
+
+double LineL(const Pair& p1, const Pair& p2, const Pair& p) {
+	return ((p.second - p1.second) * (p2.first - p1.first) -
+		(p2.second - p1.second) * (p.first - p1.first));
+}
+
+// Функция находит по какю сторону относительно прямой p1p2 лежит точка p
+int detSide(const Pair& p1, const Pair& p2, const Pair& p)
+{
+	// Данное значение определяет по какую сторону от прямой p1p2 лежит точка p
+	double side = LineL(p1, p2, p);
+
+	if (side > 0)
+		return 1;
+
+	if (side < 0)
+		return -1;
+
+	return 0;
+}
+
+	// Функия для записи координат точек из set<Pair> pr в текстовый файл
+void WriteToFile(set<Pair>& pr, const string& path) {
+
+	// Создадим ofstream для записи в файл
+	ofstream fin;
+	fin.open(path);
+	fin << pr.size();
+
+	for (auto it : hull)
+		fin << "\n" << it.first << " " << it.second;
+
+	fin.close();
+}
+
+// Функия для считывания координат точек из текстого файла в vector<Pair> pr
+void ReadFromFile(vector<Pair>& pr, const string& path) {
+	// Создадим ifstream для чтения файла
+	ifstream fout;
+
+	fout.open(path);
+	int n = 0;
+	fout >> n;
+	pr.resize(n + 1);
+	// cout << n << " " << pr.size();
+	n = 0;
+	// Записываем данные в vector<Pair> pr
+	while (fout) {
+		fout >> pr[n].first >> pr[n].second;
+		++n;
+	}
+
+	fout.close();
+}
+
+// Функция для вывода результата в консоль
+void printHull() {
+	cout << "The points in Convex Hull are: " << hull.size();
+
+	for (auto it : hull)
+	{
+		cout << "\n(" << it.first << ", " << it.second << ") ";
+	}
+}
---
+++ quickHull.h
@@ -0,0 +1,110 @@
+﻿#include "lib.h"
+
+struct Quick {
+private:
+
+    // Функция находит расстояние от прямой p1p2 до точки p
+    double lineDist(const Pair& p1, const Pair& p2, const Pair& p) {
+
+        return fabs(LineL(p1, p2, p));
+    }
+
+    // Функция поиска точек искомой оболочки
+    void quickHull(const vector<Pair>& point, const Pair& p1, const Pair& p2, const int& side) {
+
+        // текущий индикатор
+        int currentI = -1;
+        // максимальное расстояние
+        double maxDist = 0;
+        {
+        // Расстояние от прямой p1p2 до точки point[i]
+        double dist = 0;
+
+            // Найдем точку на максимальном расстояни от прямой p1p2, находящуюся на стороне side
+            for (int i = 0; i < point.size(); i++) {
+
+                // Расстояние от прямой p1p2 до точки point[i]
+                dist = lineDist(p1, p2, point[i]);
+
+                if (detSide(p1, p2, point[i]) == side && dist > maxDist) {
+
+                    currentI = i;
+                    maxDist = dist;
+                }
+            }
+        }
+
+        // Если не одной точки не найдено, добавляем p1 и p2 в оболочку hull
+        if (currentI == -1) {
+            //cout << p1.first << " " << p1.second << endl;
+            //cout << p2.first << " " << p2.second << endl;
+
+            hull.insert(p1);
+            hull.insert(p2);
+
+            return;
+        }
+
+        // Рекурсия двух частей, разделенных по point[currentI]
+
+        quickHull(point, point[currentI], p1, -detSide(point[currentI], p1, p2));
+
+        quickHull(point, point[currentI], p2, -detSide(point[currentI], p2, p1));
+
+        //cout << "-------------------\n";
+    }
+
+    // Функция вычисления точек оболочки
+    void calcHull(const vector<Pair>& point) {
+
+        if (point.size() < 3) {
+
+            cout << "Convex hull not possible, n < 3\n";
+            return;
+        }
+
+        int min = 0, max = 0;
+
+        for (int i = 1; i < point.size(); ++i) {
+
+            //Найдем самую левую точку min и самую правую точку max
+            if (point[i].first < point[min].first)
+
+                min = i;
+
+            if (point[i].first > point[max].first)
+
+                max = i;
+
+            // Если таких несколько, выберем среди таких нижнюю и верхнюю соответственно
+            if ((fabs(point[i].first - point[min].first) < accuracy) && (point[i].second - point[min].second) < 0)
+                min = i;
+
+            if ((fabs(point[i].first - point[max].first) < accuracy) && (point[i].second - point[max].second) > 0)
+                max = i;
+        }
+
+        // Найдем точки оболочки на одной стороне прямой a[min] a[max] с помощью рекурсии
+        quickHull(point, point[min], point[max], 1);
+
+        // Найдем точки оболочки на противоположной стороне прямой a[min] a[max] с помощью рекурсии
+        quickHull(point, point[min], point[max], -1);
+    }
+
+    void exe() {
+        hull.clear();
+        std::cout << "Quick:\n";
+        vector<Pair> arr = { };
+        ReadFromFile(arr, inFileQuick);
+        calcHull(arr);
+        WriteToFile(hull, outFileQuick);
+        printHull();
+    }
+
+public:
+
+    void operator()() {
+        this->exe();
+    }
+
+};
